e96b9a41cf72941f73b339d488beeaafb1b2a2d7
bachiito <riveraabraham332@gmail.com>
2023-02-09 21:08:37 -0400

    build 1x1 as space fillers, and hover transtion on images

--- index.js
+++ index.js
@@ -2,20 +2,53 @@ const gallery = document.querySelector(".gallery");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".close");
 
+const oneByOnes = Array.from({ length: 21 }, () => [
+  randomNumber(1),
+  randomNumber(1),
+]);
+
 const itemsSize = Array.from({ length: 50 }, () => [
   randomNumber(4),
   randomNumber(4),
-]);
+]).concat(oneByOnes);
 
 const gridHTML = itemsSize.map(generateGridItem).join("");
 gallery.innerHTML = gridHTML;
 
+const galleryItems = Array.from(document.querySelectorAll(".gallery__item"));
+galleryItems.map(showModal);
+galleryItems.map(imgOverlay);
+
+closeBtn.addEventListener("click", () => modal.close());
+
+function showModal(item) {
+  item.addEventListener("click", () => {
+    const imgCopy = item.lastElementChild.cloneNode(true);
+
+    if (modal.childElementCount > 1) {
+      modal.removeChild(modal.lastElementChild);
+    }
+
+    modal.appendChild(imgCopy);
+    modal.showModal();
+  });
+}
+
 function generateGridItem([col, row]) {
   return `
-    <div class="item col-${col} row-${row}">
-      <img src="assets/${randomNumber(12)}.jpg">
+    <div class="gallery__item col-${col} row-${row}">
+      <img src="assets/${randomNumber(12)}.jpg" class="gallery__img">
     </div>
   `;
+}
+
+function imgOverlay(item) {
+  item.addEventListener("mouseover", () => {
+    document.documentElement.style.setProperty(
+      "--img-height",
+      `-${item.offsetHeight}px`
+    );
+  });
 }
 
 function randomNumber(limit) {
